8ed63a23043a6c12d9d7a6b2d78025d5bd60cfbb
olowe christian <olowechristian@gmail.com>
2024-10-10 00:57:13 +0100

    created an object for my operator functions

---
+++ script.js
@@ -0,0 +1,7 @@
+const operation = {
+    add: function(num1, num2) { return num1 + num2; },
+    subtract: function(num1, num2) { return num1 - num2; },
+    multiply: function(num1, num2) { return num1 * num2; },
+    divide: function(num1, num2) { if (num2 === 0) { return "Error: Division by zero is undefined."; }
+    return num1 / num2; },
+} // operation.add(num1, num2);
