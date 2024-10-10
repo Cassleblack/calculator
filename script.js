4546c701b5841d8ac74344ab301609f100fb8147
olowe christian <olowechristian@gmail.com>
2024-10-10 01:15:09 +0100

    added an operates function for the calculation and arithmetic function for each operators

--- script.js
+++ script.js
@@ -4,4 +4,26 @@ const operation = {
     multiply: function(num1, num2) { return num1 * num2; },
     divide: function(num1, num2) { if (num2 === 0) { return "Error: Division by zero is undefined."; }
     return num1 / num2; },
+    operates: function(num1, operator, num2) {
+        switch (operator) {
+            case '+':
+                return this.add(num1, num2);
+            case '-':
+                return this.subtract(num1, num2);
+            case '*':
+                return this.multiply(num1, num2);
+            case '/':
+                return this.divide(num1, num2);
+            default:
+                return "Invalid operator!";
+        }
+    }
 } // operation.add(num1, num2);
+
+const operators = ['+', '-', '*', '/', '='];
+let userNum1;
+let userNum2; 
+let userOperator;
+
+
+
